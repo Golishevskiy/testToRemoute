651564c2303265cba381fe813e56d84bc8092df9
Petro Golishevskiy <golishevskiypetro@ukr.net>
2022-03-13 17:44:03 +0200

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Petro Golishevskiy on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
